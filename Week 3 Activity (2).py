6a8638e3178c31fcd47228eec99d958d23f54972
Rachell Ann Y. Pilo <rachellannypilo0824@gmail.com>
2026-02-10 14:43:54 +0000

    Week 3 Activity (2) from ppt

---
+++ Week 3 Activity (2).py
@@ -0,0 +1,23 @@
+while True:
+    password = input ("Enter Password: ")
+
+    has_letter = False
+    has_number = False
+
+    for char in password:
+        if char.isalpha():
+            has_letter = True
+        
+        if char.isdigit():
+            has_number = True
+    
+    if has_letter and has_number:
+        print("\n------------------------------")
+        print("      Password Accepted.")
+        print("------------------------------")
+        break
+
+    else:
+        print("------------------------------")
+        print("Invalid Password. Try Again.")
+        print("------------------------------")
