110d2e7d733a514905fb662da3bf81745f4a404e
Rachell Ann Y. Pilo <rachellannypilo0824@gmail.com>
2026-02-10 22:14:56 +0800

    Week 2 Lab Activity

---
+++ Week 2 Lab Activity.py
@@ -0,0 +1,25 @@
+# Create variables
+
+fullname = "Rachell Ann Y. Pilo"          # String
+age = 19                                  # Integer
+height = 1.54                             # Float
+currently_working = False                 # Boolean
+
+# Display values and their data types
+
+print("Fullname:", fullname)
+print(type(fullname))
+
+
+print("\nAge:", age)
+print(type(age))
+
+
+print("\nHeight in meters:", height)
+print(type(height))
+
+
+print("\nCurrently working?", currently_working)
+print(type(currently_working))
+
+# This program demonstrates variable creation and data types.
