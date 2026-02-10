1750d2aa16cc71335fc65da7ee8202014b022d8e
Rachell Ann Y. Pilo <rachellannypilo0824@gmail.com>
2026-02-10 22:18:49 +0800

    Week 3 Lab Activity

---
+++ Week 3 Lab Activity.py
@@ -0,0 +1,10 @@
+name = input("\nPlease enter your name: ")
+age = input("\nPlease enter your age: ")
+
+age = int(age)
+
+print("\n-----------------------GREETINGS!-------------------------\n")
+
+print("Hello, " + name + "!\n \nYou are " + str(age) + " years old.\n")
+
+#This is a program that asks the user to enter their name and age.
