08b31cb00ceefcebbbe43c6136151df24ca114dd
Rachell Ann Y. Pilo <rachellannypilo0824@gmail.com>
2026-03-08 09:46:50 +0800

    Basic calculator

---
+++ Assignment Basic Calc.py
@@ -0,0 +1,12 @@
+#Assignment: A Basic calculator
+
+Fnum1 = float(input("First number: "))
+operation = input("\nOperation (+,-,*,/): ")
+Fnum2 = float(input("\nSecond number: "))
+
+if operation == "+": print("\nAnswer:", Fnum1 + Fnum2)
+elif operation == "-": print("\nAnswer:", Fnum1 - Fnum2)
+elif operation == "*": print("\nAnswer:", Fnum1 * Fnum2)
+elif operation == "/": print("\nAnswer:", Fnum1 / Fnum2)
+else: 5
+print("\nInvalid operation. Please choose from +, -, *, or /.")
