6e74b7bfeba994bd934f047770fb781a14ad26a0
Rachell Ann Y. Pilo <rachellannypilo0824@gmail.com>
2026-03-08 09:39:18 +0800

    Remove space

--- Assignment Basic Calc.py
+++ Assignment Basic Calc.py
@@ -8,5 +8,4 @@ if operation == "+": print("\nAnswer:", Fnum1 + Fnum2)
 elif operation == "-": print("\nAnswer:", Fnum1 - Fnum2)
 elif operation == "*": print("\nAnswer:", Fnum1 * Fnum2)
 elif operation == "/": print("\nAnswer:", Fnum1 / Fnum2)
-
 else: print("\nInvalid operation. Please choose from +, -, *, or /.")
