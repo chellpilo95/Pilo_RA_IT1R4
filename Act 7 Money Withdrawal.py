d16d2ca5d2e90f50677833a32a0f9d605820ee4a
Rachell Ann Y. Pilo <rachellannypilo0824@gmail.com>
2026-04-11 17:53:04 +0800

    hhe

--- Act 7 Money Withdrawal.py
+++ Act 7 Money Withdrawal.py
@@ -1,39 +1,39 @@
-Amt_balance = 10500  
+amount_balance = 10500  
 
 def menu():   
     print("\n| Money Withdrawal System |\n")
     print("1. Withdraw Money\n2. Check Balance\n3. Exit")
     return input("\nEnter your choice: ")
 
-def withdraw(balance):  
+def withdraw(amount_balance):  
     try:  
         amount = float(input("\nEnter amount to withdraw: "))
         if amount <= 0:
             print("\nAmount must be greater than 0.")
-        elif amount > balance:
+        elif amount > amount_balance:
             print("\nInsufficient funds!")
             
             while True:
                 option = input("\nOptions:\n1. Re-enter amount\n2. Check balance\n3. Exit\n\nEnter your choice: ")
-                if option == "1": return balance
-                elif option == "2": print(f"\nYour Current balance is {balance}.")
+                if option == "1": return amount_balance
+                elif option == "2": print(f"\nYour Current balance is {amount_balance}.")
                 elif option == "3": exit("\nExiting system...")
                 else: print("\nInvalid option! Please try again.")
         else:
-            balance -= amount
-            print(f"\nWithdrawal successful! Your remaining balance is {balance}.")
+            amount_balance -= amount
+            print(f"\nWithdrawal successful! Your remaining balance is {amount_balance}.")
             
     except ValueError:
         print("\nInvalid input! Please enter a valid number.")
         
-    return balance
+    return amount_balance
 
 while True:
     choice = menu()
     if choice == "1":
-        balance = withdraw(balance)
+        amount_balance = withdraw(amount_balance)
     elif choice == "2":
-        print(f"\nYour Current balance is {balance}.")
+        print(f"\nYour Current balance is {amount_balance}.")
     elif choice == "3":
         print("\nExiting...Thank you for using the system!")
         break
