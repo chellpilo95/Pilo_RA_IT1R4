bbbec794304aa7fde604ce1c7283dbe6bb8d14e8
Rachell Ann Y. Pilo <rachellannypilo0824@gmail.com>
2026-04-15 19:45:45 +0800

    Week 8 LAb Activity: File Handling in Python

---
+++ week 8 Lab Activity.py
@@ -0,0 +1,46 @@
+try:
+    f = open("message.txt", "x")
+    print("File created successfully!")
+
+except FileExistsError:
+    print("File already exists!")
+
+while True:
+    print("\nWelcome to Messaging App!\n")
+    print("1. Send Message")
+    print("2. View Messages")
+    print("3. Exit")
+    
+    choice = input("\nEnter your choice: ")
+    
+    if choice == '1':
+        message = input("\nEnter your message: ")
+        try:
+            with open("message.txt", "a") as f:
+                f.write(message + "\n")
+            print("\nMessage sent successfully!")
+
+        except Exception as e:
+            print("\nError Sending the Message: ")
+    
+    elif choice == '2':
+        try:
+            with open("message.txt", "r") as f:
+                messages = f.readlines()
+                if messages:
+                    print("\nMessages:\n")
+                    for msg in messages:
+                        print(msg.strip())
+                else:
+                    print("No messages found.")
+
+        except Exception as e:
+            print("Error reading the message: ")
+    
+    elif choice == '3':
+        print("Exiting the program....")
+        break
+    
+    else:
+        print("Invalid choice. Please enter again.")
+
